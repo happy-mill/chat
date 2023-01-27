8c18987912ebfbc117f14b7b617abedc1c5e68be
Igor <gunjeek@gmail.com>
2023-01-27 13:30:49 +0300

    refactor main.py

--- main.py
+++ main.py
@@ -13,9 +13,6 @@ tg_api_key = os.environ.get('TG_KEY')
 bot = aiogram.Bot(token=tg_api_key)
 dp = Dispatcher(bot)
 
-# context variable to store information about the current conversation
-context = {}
-
 @dp.message_handler(commands=['start'])
 async def start_command(message: Message):
     await message.reply("Hello! How can I help you today?")
@@ -25,15 +22,13 @@ async def echo_message(message: Message):
     # use the OpenAI API to generate a response
     response = openai.Completion.create(
         engine="text-davinci-003",
-        prompt=f"{context.get('prompt', '')} {message.text}",
+        prompt=message.text,
         temperature=0.5,
         max_tokens=2048,
         top_p=1,
         frequency_penalty=1,
         presence_penalty=1
     )
-    # update the context with the current message
-    context['prompt'] = f"{context.get('prompt', '')} {message.text}"
     # send the response
     await message.reply(response.choices[0].text)
 
